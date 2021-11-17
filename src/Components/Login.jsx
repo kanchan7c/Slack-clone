ad470da4d26237f34d1c67ad05dce311f45cbb1b
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2021-11-17 12:52:04 +0530

    created login card and styled it

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,18 @@
+import { Button } from "@mui/material";
 import React from "react";
 import "./login.css";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="login">
+      <div className="login_container">
+        <img src="../favicon.ico" alt="Slack Logo" />
+        <h1>Sign in to Web Genie</h1>
+        <p>webgenie.slack.com</p>
+        <Button>Sign in with Google</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
