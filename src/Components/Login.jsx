859d4e436ae61ee96eacbbdb4c9e64764fe4a841
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2021-11-17 13:15:13 +0530

    setting up use Context to globally access users

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,15 +1,27 @@
 import { Button } from "@mui/material";
 import React from "react";
 import "./login.css";
+import { auth, provider } from "../firebase";
 
 const Login = () => {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
   return (
     <div className="login">
       <div className="login_container">
         <img src="../favicon.ico" alt="Slack Logo" />
         <h1>Sign in to Web Genie</h1>
         <p>webgenie.slack.com</p>
-        <Button>Sign in with Google</Button>
+        <Button onClick={signIn}>Sign in with Google</Button>
       </div>
     </div>
   );
